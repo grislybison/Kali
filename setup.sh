738c634990a4377aff289a7c2db654c60352e950
grislybison <106167089+grislybison@users.noreply.github.com>
2022-05-24 21:38:33 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+echo ''>> ~/.zshrc
 echo "alias update='apt update ; apt -y upgrade ; apt -y dist-upgrade ; apt -y autoremove ; apt -y autoclean'" >> ~/.zshrc
 echo "alias n='echo -n 'External IP: ' && curl ifconfig.me ; echo ; echo -n 'VM IP: ' && hostname -I ; echo; netstat -antp; echo; ping -c 3 8.8.8.8'" >> ~/.zshrc
 echo "source ~/.zshrc"
