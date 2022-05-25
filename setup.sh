401dbb1b7f3a0a1875620aa045e7211e56a921e1
grislybison <106167089+grislybison@users.noreply.github.com>
2022-05-24 22:54:26 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
+# Creates space above new aliases in .zshrc file and labels section
 echo ''>> ~/.zshrc
+echo 'User created aliases' >> ~/.zshrc
+# adds alias for quick update via CLI
 echo "alias update='apt update ; apt -y upgrade ; apt -y dist-upgrade ; apt -y autoremove ; apt -y autoclean'" >> ~/.zshrc
-echo 'alias n="echo -n '"'External IP: '"' && curl ifconfig.me; echo; echo -n '"'VM IP: '"'; hostname -I | awk '"'{print "'$1'"}'"'; echo; netstat -antp; ping -c 3 8.8.8.8"' >> ~/.zshrc
-echo "source ~/.zshrc"
+#shows external IP, VM IP, runs netstat, and pings google DNS
+echo 'alias n="echo -n '"'External IP: '"' && curl ifconfig.me; echo; echo -n '"'VM IP: '"'; hostname -I | awk '"'{print "'$1'"}'"'; echo; netstat -antp; echo; ping -c 3 8.8.8.8"' >> ~/.zshrc
+#reloads .zshrc 
+echo 'source ~/.zshrc'
